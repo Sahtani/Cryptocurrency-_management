369c5253342f2a6a38ce9d4af55a6fc0284095db
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-01-03 23:39:39 +0100

    display coins and add watchlist

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,31 +1,40 @@
 <?php
-  class Pages extends Controller {
-    public function __construct(){
-     
-    }
-    
-    public function index(){
-      $data = [
-        'title' => 'TraversyMVC',
-      ];
-      
-     
-      $this->view('pages/index', $data);
-    }
-
-    public function about(){
-      $data = [
-        'title' => 'About Us'
-      ];
-
-      $this->view('pages/about', $data);
-    }
-
-    publiC function dashboard(){
-      $this->view('pages/dashboard');
-    }
-
-    public function Watchlist(){
-      $this->view('pages/watchlist');
-    }
+class Pages extends Controller
+{
+  public function __construct()
+  {
   }
+
+  public function index()
+  {
+    $data = [
+      'title' => 'TraversyMVC',
+    ];
+
+
+    $this->view('pages/index', $data);
+  }
+
+  public function about()
+  {
+    $data = [
+      'title' => 'About Us'
+    ];
+
+    $this->view('pages/about', $data);
+  }
+
+  public function dashboard()
+  {
+    $this->model('User');
+    $data = new User();
+    $row = $data->displayCoin();
+    $data = array('row' => $row);
+    $this->view('pages/dashboard', $data);
+  }
+
+  public function Watchlist()
+  {
+    $this->view('pages/watchlist');
+  }
+}
